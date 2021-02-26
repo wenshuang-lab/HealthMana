ed28587cf57d2289b66be34ffc05fd8be877baca
wenshuang-lab <78012149+wenshuang-lab@users.noreply.github.com>
2021-02-26 20:33:47 +0800

    Add files via upload

---
+++ data/data_discovery_next.js
@@ -0,0 +1,112 @@
+/**
+ * Created by Rebecca_Han on 16/10/27.
+ */
+
+module.exports={
+
+};
+
+var next = {
+    "id": 4,
+    "data": [
+        {
+            "question_id": 1,
+            "answer_id": 1,
+            "feed_source_id": 23,
+            "feed_source_name": "大米",
+            "feed_source_txt": "分享",
+            "feed_source_img": "../../images/icon1.jpeg",
+            "question": "前端面试策略（一）-- 学会总结",
+            "answer_ctnt": "前端求职虽重在知识的积累，但是如果不学会推销自己，包装自己，好的工作可遇而抓不到，因此拥有好的面试技巧能够让你在前段道路上少走很多弯路，所以我总结了这篇文章，希望可以帮助那些前端求职者...",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 2,
+            "answer_id": 2,
+            "feed_source_id": 23,
+            "feed_source_name": "大米",
+            "feed_source_txt": "分享",
+            "feed_source_img": "../../images/icon1.jpeg",
+            "question": "前端面试策略（二）-- 学会总结",
+            "answer_ctnt": "前端求职虽重在知识的积累，但是如果不学会推销自己，包装自己，好的工作可遇而抓不到，因此拥有好的面试技巧能够让你在前段道路上少走很多弯路，所以我总结了这篇文章，希望可以帮助那些前端求职者...",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 3,
+            "answer_id": 3,
+            "feed_source_id": 23,
+            "feed_source_name": "大米",
+            "feed_source_txt": "分享",
+            "feed_source_img": "../../images/icon1.jpeg",
+            "question": "前端面试策略（三）-- 学会总结",
+            "answer_ctnt": "前端求职虽重在知识的积累，但是如果不学会推销自己，包装自己，好的工作可遇而抓不到，因此拥有好的面试技巧能够让你在前段道路上少走很多弯路，所以我总结了这篇文章，希望可以帮助那些前端求职者...",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 4,
+            "answer_id": 4,
+            "feed_source_id": 23,
+            "feed_source_name": "大米",
+            "feed_source_txt": "分享",
+            "feed_source_img": "../../images/icon1.jpeg",
+            "question": "前端面试策略（四）-- 学会总结",
+            "answer_ctnt": "前端求职虽重在知识的积累，但是如果不学会推销自己，包装自己，好的工作可遇而抓不到，因此拥有好的面试技巧能够让你在前段道路上少走很多弯路，所以我总结了这篇文章，希望可以帮助那些前端求职者...",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 5,
+            "answer_id": 5,
+            "feed_source_id": 23,
+            "feed_source_name": "大米",
+            "feed_source_txt": "分享",
+            "feed_source_img": "../../images/icon1.jpeg",
+            "question": "前端面试策略（五）-- 学会总结",
+            "answer_ctnt": "前端求职虽重在知识的积累，但是如果不学会推销自己，包装自己，好的工作可遇而抓不到，因此拥有好的面试技巧能够让你在前段道路上少走很多弯路，所以我总结了这篇文章，希望可以帮助那些前端求职者...",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 6,
+            "answer_id": 6,
+            "feed_source_id": 23,
+            "feed_source_name": "大米",
+            "feed_source_txt": "分享",
+            "feed_source_img": "../../images/icon1.jpeg",
+            "question": "前端面试策略（六）-- 学会总结",
+            "answer_ctnt": "前端求职虽重在知识的积累，但是如果不学会推销自己，包装自己，好的工作可遇而抓不到，因此拥有好的面试技巧能够让你在前段道路上少走很多弯路，所以我总结了这篇文章，希望可以帮助那些前端求职者...",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 7,
+            "answer_id": 7,
+            "feed_source_id": 23,
+            "feed_source_name": "大米",
+            "feed_source_txt": "分享",
+            "feed_source_img": "../../images/icon1.jpeg",
+            "question": "前端面试策略（七）-- 学会总结",
+            "answer_ctnt": "前端求职虽重在知识的积累，但是如果不学会推销自己，包装自己，好的工作可遇而抓不到，因此拥有好的面试技巧能够让你在前段道路上少走很多弯路，所以我总结了这篇文章，希望可以帮助那些前端求职者...",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+        {
+            "question_id": 8,
+            "answer_id": 8,
+            "feed_source_id": 23,
+            "feed_source_name": "大米",
+            "feed_source_txt": "分享",
+            "feed_source_img": "../../images/icon1.jpeg",
+            "question": "前端面试策略（八）-- 学会总结",
+            "answer_ctnt": "前端求职虽重在知识的积累，但是如果不学会推销自己，包装自己，好的工作可遇而抓不到，因此拥有好的面试技巧能够让你在前段道路上少走很多弯路，所以我总结了这篇文章，希望可以帮助那些前端求职者...",
+            "good_num": "112",
+            "comment_num": "18"
+        },
+
+    ]
+
+}
+module.exports.next = next;
